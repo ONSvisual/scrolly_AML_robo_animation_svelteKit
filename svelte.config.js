5e69a8238cc5971c7fc096cceb827ec4ec4eca09
Tim Kershaw <ctkershaw@gmail.com>
2022-03-27 10:46:54 +0100

    added netlify adapter to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
 import static_adapter from '@sveltejs/adapter-static';
+
+import adapter from '@sveltejs/adapter-netlify';
+
+
+
+
+
 
 const production = process.env.NODE_ENV === 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: static_adapter(),
-		//adapter: adapter(),
+		//adapter: static_adapter(),
+		adapter: adapter(),
 
 		files: {
 			assets: 'static',
